9e539a7bf464afec52e42feaadddaa16c270e073
Naveen Reddy <naveenrg914@gmail.com>
2021-09-26 13:33:08 -0500

    adding thymeleaf dependency to the pom

--- src/main/java/com/naveen/contactentrysystem/controller/ContactDetailsController.java
+++ src/main/java/com/naveen/contactentrysystem/controller/ContactDetailsController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -21,6 +22,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:4200")
 @RequestMapping(value = "/contacts")
 public class ContactDetailsController {
 

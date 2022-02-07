c0b5f11402db90d8b7bb49841e980c5dc9dab1ca
Naveen Reddy <naveenrg914@gmail.com>
2022-02-06 21:57:49 -0600

    minor change

---
+++ src/main/java/com/naveen/contactentrysystem/util/ContactEntrySystemException.java
@@ -0,0 +1,11 @@
+package com.naveen.contactentrysystem.util;
+
+public class ContactEntrySystemException extends RuntimeException {
+
+	ContactEntrySystemException(String errorMessage, Throwable err) {
+
+		super(errorMessage, err);
+
+	}
+
+}
